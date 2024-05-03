adeef95a5551078c2d0979ac4225d631f13d55ae
Mokshith <garipallymokshith@gmail.com>
2024-05-03 14:12:50 +0530

    latest change

--- routers/ExpertDetails.js
+++ routers/ExpertDetails.js
@@ -3,10 +3,10 @@ const router = require('./ExpertRoutes')
 
 const ExpertDetails = require('../controllers/ExpertDetails')
 
-router.get('/expertDetails/:id', ExpertDetails.getDetails);
+// router.get('/expertDetails/:id', ExpertDetails.getDetails);
 router.get('/expertDetails', ExpertDetails.getAllExpertDetails);
 router.post('/createExpertDetails', ExpertDetails.createExpertProfile);
-router.get('/expertDetailid/:id', ExpertDetails.getDetailsid);
+router.get('/expertDetails/:id', ExpertDetails.getDetailsid);
 
 
 module.exports = router;
